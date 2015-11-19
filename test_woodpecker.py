bb1b6b46737f5278a7c8d9d6cc4e1cc5008def9e
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2015-11-18 23:09:16 -0200

    Added token unrecognized closes #1

--- test_woodpecker.py
+++ test_woodpecker.py
@@ -2,6 +2,7 @@ import unittest
 
 from unittest.mock import patch
 from woodpecker import Woodpecker
+from woodpecker import TokenUnrecognized
 
 
 class AssertMixin():
@@ -10,20 +11,24 @@ class AssertMixin():
         """
 
         """
-        filtered = list(filter(lambda t: t[1]==token_type, tokens))
+        filtered = list(filter(lambda t: t[1] == token_type, tokens))
         count = len(filtered)
 
         self.assertEquals(count, expected)
 
 
 class TestWoodpecker(unittest.TestCase, AssertMixin):
- 
+
     def setUp(self):
         pass
 
     def __f(self):
         """ Factory a object Woodpecker """
         return Woodpecker('**file**')
+
+    def __asrt(self, token, type):
+        type_token = self.__f().get_type_token(token)
+        self.assertEquals(type_token, type)
 
     def __should_be(self, actual, expected, func, mock):
         """
@@ -35,32 +40,35 @@ class TestWoodpecker(unittest.TestCase, AssertMixin):
         self.assertEquals(code, expected)
 
     def __to_be(self, expected, value):
-        type_token = self.__f().get_type_token(value) 
+        type_token = self.__f().get_type_token(value)
         self.assertEquals(type_token, expected)
- 
+
     @patch("woodpecker.Woodpecker.read_content_file")
     def test_remove_multiple_spaces(self, mock):
 
-        self.__should_be("  Your code   is   bad    ", "Your code is bad", "remove_multiple_spaces", mock)
-        self.__should_be(" A bug in my       code", "A bug in my code", "remove_multiple_spaces", mock)
-        self.__should_be("Rewrite    a   bad code", "Rewrite a bad code", "remove_multiple_spaces", mock)
-
+        self.__should_be("  Your code   is   bad    ",
+                         "Your code is bad", "remove_multiple_spaces", mock)
+        self.__should_be(
+            " A bug in my       code", "A bug in my code", "remove_multiple_spaces", mock)
+        self.__should_be(
+            "Rewrite    a   bad code", "Rewrite a bad code", "remove_multiple_spaces", mock)
 
     @patch("woodpecker.Woodpecker.read_content_file")
-    def test_remove_empty_lines(self, mock): 
+    def test_remove_empty_lines(self, mock):
         code = "Code review?\t\n\tAin't nobody\n\n\ngot time\rfor that\r\n\n."
-        self.__should_be(code, "Code review?   Ain't nobody   got time for that   .", "remove_empty_lines", mock)
+        self.__should_be(
+            code, "Code review?   Ain't nobody   got time for that   .", "remove_empty_lines", mock)
 
     @patch("woodpecker.Woodpecker.read_content_file")
     def test_remove_comment(self, mock):
-        self.__should_be("--Pushed new code--", "", "remove_comment", mock) 
-        self.__should_be("-- Wall of code --", "", "remove_comment", mock) 
+        self.__should_be("--Pushed new code--", "", "remove_comment", mock)
+        self.__should_be("-- Wall of code --", "", "remove_comment", mock)
 
-        self.__should_be("-- That source code -- So hot rigth now", "So hot rigth now", "remove_comment", mock) 
+        self.__should_be("-- That source code -- So hot rigth now",
+                         "So hot rigth now", "remove_comment", mock)
 
     @patch("woodpecker.Woodpecker.read_content_file")
     def test_get_type_token(self, mock):
-
         """ Check token type 'integer' """
         self.__to_be("integer", "0")
         self.__to_be("integer", "101")
@@ -87,13 +95,12 @@ class TestWoodpecker(unittest.TestCase, AssertMixin):
         self.__to_be("keyword", "igual")
         self.__to_be("keyword", "se")
         self.__to_be("keyword", "fim")
-        
+
         """ Check token type 'identifiers' """
         self.__to_be("idt", "split")
         self.__to_be("idt", "print")
         self.__to_be("idt", "calc")
         self.__to_be("idt", "sum")
-
 
     @patch("woodpecker.Woodpecker.read_content_file")
     def test_generate_token(self, mock):
@@ -105,21 +112,21 @@ class TestWoodpecker(unittest.TestCase, AssertMixin):
             variavel numero :
 
             imprimir #Esse é o meu primeiro analisador léxico# :
-            
+
             para numero ate 100 faca :
             {
                 exibir numero :
             }
 
             --check the password--
-            senha = 1234 :          
-            
+            senha = 1234 :
+
             saldo = -23.32 :
 
             se senha igual 1234 :
             {
                 exibir #Login efetuado com suscesso# :
-            }   
+            }
         fim
 
         """
@@ -136,6 +143,55 @@ class TestWoodpecker(unittest.TestCase, AssertMixin):
         self.assertCountTokenType(tokens, 'idt-start-block', 2)
         self.assertCountTokenType(tokens, 'idt-end-block', 2)
         self.assertCountTokenType(tokens, 'assignment', 2)
+
+    @patch("woodpecker.Woodpecker.read_content_file")
+    def test_regex_type_token_integer(self, mock):
+
+        def asrt(number):
+            self.__asrt(number, 'integer')
+
+        asrt('0')
+        asrt('-10')
+        asrt('+10')
+        asrt('10')
+        self.assertRaises(TokenUnrecognized, lambda: asrt('13a'))
+
+    @patch("woodpecker.Woodpecker.read_content_file")
+    def test_regex_type_token_float(self, mock):
+
+        def asrt(float):
+            self.__asrt(float, 'float')
+
+        asrt('0.0')
+        asrt('+10.0')
+        asrt('-10.0')
+        asrt('10.0')
+        asrt('10.10')
+        asrt('10.10')
+        asrt('0.10')
+        self.assertRaises(TokenUnrecognized, lambda: asrt('1.22E'))
+        self.assertRaises(TokenUnrecognized, lambda: asrt('13a.22'))
+
+    @patch("woodpecker.Woodpecker.read_content_file")
+    def test_regex_type_token_idenifier(self, mock):
+
+        def asrt(idt):
+            self.__asrt(idt, 'idt')
+
+        asrt('count')
+        asrt('c123')
+        asrt('c123c')
+        asrt('count')
+        asrt('Count')
+        asrt('cOUNT')
+
+        self.assertRaises(TokenUnrecognized, lambda: asrt('1count'))
+        self.assertRaises(TokenUnrecognized, lambda: asrt('_count'))
+        self.assertRaises(TokenUnrecognized, lambda: asrt('#count'))
+        self.assertRaises(TokenUnrecognized, lambda: asrt(' count'))
+        self.assertRaises(TokenUnrecognized, lambda: asrt('!'))
+        self.assertRaises(TokenUnrecognized, lambda: asrt('.'))
+        self.assertRaises(TokenUnrecognized, lambda: asrt(';'))
 
 if __name__ == "__main__":
     unittest.main()
--- woodpecker.py
+++ woodpecker.py
@@ -1,11 +1,16 @@
 import re
 
+
+class TokenUnrecognized(Exception):
+    pass
+
+
 class Woodpecker():
 
     def __init__(self, filename):
         self.source_code = self.read_content_file(filename)
-        
-    def code(self):     
+
+    def code(self):
         return self.source_code
 
     def tokens(self):
@@ -17,13 +22,13 @@ class Woodpecker():
             print('<{}, {}>'.format(t, tp), end=' ')
 
     def read_content_file(self, filename):
-        
+
         with open(filename) as f:
             content = f.read()
 
         return content
 
-    def remove_multiple_spaces(self): 
+    def remove_multiple_spaces(self):
         DOUBLE_SPACE = ' +'
 
         self.source_code = re.sub(DOUBLE_SPACE, ' ', self.source_code)
@@ -38,7 +43,7 @@ class Woodpecker():
 
     def remove_comment(self):
         COMMENT = '--.*?--'
-        
+
         self.source_code = re.sub(COMMENT, ' ', self.source_code).strip()
         return self.source_code
 
@@ -50,13 +55,17 @@ class Woodpecker():
         ASSIGNMENT = "="
 
         def is_integer(s):
-            return re.match("[-+]?\d+$", s) is not None
+            return re.match("^[-+]?\d+$", s) is not None
 
         def is_float(s):
-            return re.match("[-+]?\d+\.\d+?$", s) is not None
-        
-        KEYWORD = ['woodpecker', 'inicio', 'variavel', 'para', 'ate', 'faca', 'igual', 'se', 'fim']
-        
+            return re.match("^[-+]?\d+\.\d+?$", s) is not None
+
+        def is_identifier(s):
+            return re.match("^[A-Za-z](\w+)[A-Za-z0-9]$", s) is not None
+
+        KEYWORD = ['woodpecker', 'inicio', 'variavel',
+                   'para', 'ate', 'faca', 'igual', 'se', 'fim']
+
         if token in KEYWORD:
             return 'keyword'
         elif token[0] == "#" and token[-1] == "#":
@@ -66,15 +75,17 @@ class Woodpecker():
         elif is_float(token):
             return 'float'
         elif token == IDT_END_LINE:
-            return 'idt-end-line' 
+            return 'idt-end-line'
         elif token == IDT_START_BLOCK:
             return 'idt-start-block'
         elif token == IDT_END_BLOCK:
             return 'idt-end-block'
         elif token == ASSIGNMENT:
             return 'assignment'
-        else:
+        elif is_identifier(token):
             return 'idt'
+        else:
+            raise TokenUnrecognized('Token "{}" unrecognized'.format(token))
 
     def generate_token(self):
 
@@ -87,10 +98,6 @@ class Woodpecker():
         tokens = []
         for t in list_tokens:
             type_token = self.get_type_token(t)
-            tokens.append( (t, type_token) ) 
+            tokens.append((t, type_token))
 
         return tokens
-
-
-
-
